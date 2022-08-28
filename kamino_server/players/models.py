62b3724db74ea1e1b85d2be1fd1a608ffe800cff
tlazor <tjlazor@vt.edu>
2022-08-27 22:27:52 -0400

    Remove unnecessary fields from player

--- kamino_server/players/models.py
+++ kamino_server/players/models.py
@@ -4,8 +4,6 @@ from django.db import models
 class Player(models.Model):
     name = models.CharField(max_length=200)
     elo = models.FloatField(default=1600)
-    games_played = models.PositiveIntegerField(default=0)
-    games_won = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"{self.name} ({self.elo})"
