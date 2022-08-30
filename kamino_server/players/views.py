e87b971798760e177ec91e5ffd77f84410c57606
tlazor <tjlazor@vt.edu>
2022-08-30 18:56:54 -0400

    Add first bokeh nonsense

--- kamino_server/players/views.py
+++ kamino_server/players/views.py
@@ -2,12 +2,33 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
+from bokeh.plotting import figure
+from bokeh.embed import components
+
 from .models import Player
 from games.models import Game, Color
 
+# def index(request):
+#     player_list = Player.objects.order_by('-elo')
+#     context = {"player_list": player_list}
+#     return render(request, 'players/index.html', context)
+
+
 def index(request):
+ 
+   #create a plot
+    plot = figure(plot_width=400, plot_height=400)
+ 
+   # add a circle renderer with a size, color, and alpha
+ 
+    plot.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
+ 
+    script, div = components(plot)
+    context = {'script': script, 'div': div}
+
     player_list = Player.objects.order_by('-elo')
-    context = {"player_list": player_list}
+    context["player_list"] = player_list
+ 
     return render(request, 'players/index.html', context)
 
 
