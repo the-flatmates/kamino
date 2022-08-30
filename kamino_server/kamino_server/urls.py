62d7699ca1e5f3ad97e2efc1ce6dbbac2ea56688
tlazor <tjlazor@vt.edu>
2022-08-30 18:47:51 -0400

    Add garbo favicon

--- kamino_server/kamino_server/urls.py
+++ kamino_server/kamino_server/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon1.ico'))),
     path('players/', include('players.urls')),
     # path('stats/', include('stats.urls')),
     path('games/', include('games.urls')),
