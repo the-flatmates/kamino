63775b7a68f581b31e5e6148910f8c1b3678aa24
Eric Nguyen <eqnguyen@gmail.com>
2022-09-02 02:51:40 -0400

    Add gensim similarity functions

--- kamino/similarity.py
+++ kamino/similarity.py
@@ -1,9 +1,11 @@
 from typing import List
 
+import gensim.downloader
 import numpy as np
 import spacy
 
 nlp = spacy.load("en_core_web_lg")
+corpus = gensim.downloader.load("glove-wiki-gigaword-300")
 
 
 def pair_similarity(token1: str, token2: str) -> float:
@@ -20,7 +22,28 @@ def pair_similarity(token1: str, token2: str) -> float:
     return tokens[0].similarity(tokens[1])
 
 
-def group_similarity(words: list) -> float:
+def pairwise_similarity(word: str, words: List[str]) -> List[float]:
+    """Compute pairwise similarity between a word and a word list.
+
+    Args:
+        word (str): Word to compute pairwise similarity with
+        words (List[str]): Word list to compare
+
+    Returns:
+        List[float]: Similarity scores
+    """
+    word_token = nlp(word)
+    wl_tokens = nlp(" ".join(words))
+
+    similarities = []
+
+    for wl_token in wl_tokens:
+        similarities.append(word_token.similarity(wl_token))
+
+    return similarities
+
+
+def group_similarity(words: List[str]) -> float:
     """Compute average similarity among list of words.
 
     Args:
@@ -61,3 +84,18 @@ def most_similar(words: list, count: int = 1) -> List[str]:
 
     ms = nlp.vocab.vectors.most_similar(np.asarray([avg_vector]), n=count)
     return [str(nlp.vocab.strings[w]) for w in ms[0][0]]
+
+
+def wv_most_similar(positive: list, negative: list = [], count: int = 5) -> List[str]:
+    """Get the top N most similar words.
+
+    Args:
+        positive (list): Words that contribute positively towards similarity.
+        negative (list, optional): Words that contribute negatively towards similarity. Defaults to [].
+        count (int, optional): Number of words to return. Defaults to 5.
+
+    Returns:
+        List[str]: List of similar words.
+    """
+
+    return corpus.most_similar(positive=positive, negative=negative, topn=count)
